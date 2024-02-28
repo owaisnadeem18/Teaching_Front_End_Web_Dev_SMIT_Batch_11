dd4212a4fb1797c9ee81bce8af5222c68bfe9067
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-28 12:02:15 +0500

    Add files via upload

---
+++ Class 01 Alert ( 26-Feb-2024 )/app.js
@@ -0,0 +1,28 @@
+// Today We have to discuss chapter 01 of book smarter way to learn JS with the students of SMIT Batch 11 in the first class of JavaScript
+
+// // sweetAlert("Oops...", "Something went wrong!", "error");
+
+// confirm("Are u sure ? ", "warning");
+
+// swal({
+//   title: "Not a Good job!",
+//   text: "You clicked the dskfjsdfhsj!",
+//   icon: "warning",
+// });
+
+// swal("Hurrah !", "Hello, world!", "success");
+
+// swal({
+//   title: "Good job!",
+//   text: "You clicked the button!",
+//   icon: "success",
+//   button: "Aww yiss!",
+// });
+
+swal("Delete ", "Are u Sure ?", "warning", {
+  button: "Yes !",
+});
+
+a = alert(564);
+a = alert(typeof 564);
+// console.log(typeof a);
