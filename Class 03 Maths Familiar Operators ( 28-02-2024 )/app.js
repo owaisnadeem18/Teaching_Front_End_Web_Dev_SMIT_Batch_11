21cfd5eead596e2c8562da0b44a8e93cc84e6d77
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-02-28 22:41:49 +0500

    Add files via upload

---
+++ Class 03 Maths Familiar Operators ( 28-02-2024 )/app.js
@@ -0,0 +1,76 @@
+// Maths Familiar Operators In JavaScript
+
+// Arithmetic Operators
+
+// +, -, /, * , %
+
+// ------------------------------ 1 ------------------------------
+// For Addition (+)
+
+// var first_num = 12;
+// var second_num = 8;
+
+// console.log(first_num + second_num); // ye 2 variable key values ko add karay ga ( Or result ko add karwa k show karwaye ga , because of + arithmetic operators)
+
+// Ye aik dusray tareeqay say bhi add karwaya jaa sakta hay, iska scene ye hay k . Aap log aik nya variable banayein
+
+// for example:
+
+// let add = first_num + second_num;
+
+// console.log(add);
+
+// ------------------------------ 2 ------------------------------
+// For Subtraction (-)
+
+// var first_num = 12;
+// var second_num = 8;
+
+// console.log(first_num - second_num); // ye 2 variable key values ko subtract karay ga ( Or result ko subtract karwa k show karwaye ga , because of - arithmetic operators)
+
+// Ye aik dusray tareeqay say bhi sub karwaya jaa sakta hay, iska scene ye hay k . Aap log aik nya variable banayein
+
+// for example:
+
+// let sub = first_num - second_num;
+
+// console.log(sub);
+
+// ------------------------------ 3 ------------------------------
+// For Multiplication (*)
+
+// var first_num = 12;
+// var second_num = 8;
+
+// console.log(first_num * second_num); // ye 2 variable key values ko multipy karay ga (Or result ko multiply karwa k show karwaye ga , because of * arithmetic operators)
+
+// Ye aik dusray tareeqay say bhi multiply karwaya jaa sakta hay, iska scene ye hay k . Aap log aik nya variable banayein
+
+// for example:
+
+// let mult = first_num * second_num;
+
+// console.log(mul);
+
+// ------------------------------ 4 ------------------------------
+// For Division (/)
+
+// var first_num = 12;
+// var second_num = 8;
+
+// console.log(first_num / second_num); // ye 2 variable key values ko multipy karay ga (Or result ko multiply karwa k show karwaye ga , because of / arithmetic operators)
+
+// Ye aik dusray tareeqay say bhi Divide karwaya jaa sakta hay, iska scene ye hay k . Aap log aik nya variable banayein
+
+// for example:
+
+// let div = first_num / second_num;
+
+// console.log(div);
+
+// -------------------------- Modulus % --------------------------
+
+let pehla = 10;
+let dusra = 5;
+
+console.log(pehla % dusra); // 0 (zero) jawab aye ga , because 10/5 jb hota hay , tab remainder 0 ata hay division may , is wjah say
