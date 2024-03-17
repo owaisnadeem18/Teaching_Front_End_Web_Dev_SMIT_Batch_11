236c3bf0155e8ce7311521d567cf86a3ed80f5cf
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-17 12:44:34 +0500

    Add files via upload

---
+++ Class 05 If Else and All Comparison Operators/script.js
@@ -0,0 +1,52 @@
+// IF-Else
+// Else
+// If , Else If
+// Comaprision Operators
+
+// let a = 89;
+// let b = 83;
+
+// console.log(a <= b); // keh rha hay k kya 'a' bada hay 'b' say , if 'yes' then the result would be true , otherwise it will return 'false'
+
+// aik operator hota hay (>=) is ka mtlb k kya bada hay ya barabar hay ??
+// aik operator hota hay (<=) is ka mtlb k kya bada hay ya barabar hay ??
+// aik operator hota hay (!) is ka mtlb k ye shetaan key trah ulta hay ??
+
+// ye 'haan' bolay ga jab actually 'Na" hoga , or ye "Na" bolay ga. Jab actually 'Haan' hoga
+
+// for example
+
+// console.log(true); // true print hoga
+// console.log(!true); // fals print hoga (because true ka ulta hogya ! key wjah say . isko not operator boltay hain )
+
+// Interview Question:
+
+// Difference between === and ==
+
+// "===" ye data type bhi dekhta hay , or dono comparision honay walon key value bhi dekhta hay
+
+// "==" ye sirf dono comparision honay walon key value dekhta hay (agar same ho , tab true day deta )
+
+// Ab baat ho jaye kuch if else key
+
+// Listen , bachpan may kabhi school k test wagera hotay thay , tab ghr may abba g kehtay thay , beta agar first aye tab aapko 'bike' dilaa kar dnga or agar fail huay tab 'jutiyaan' maarun ga. Isko code krletay hain
+
+// let result = "pass";
+
+// if (result == "pass") {
+//   console.log("Beta tmhey new bike dilaun ga "); // agar pehli condition true hoti hay , tab dusri nhi dekhay ga or ye result print krday ga
+// } else {
+//   console.log("Tumhy work shop pay daalun ga ");
+// }
+
+// let age = prompt("Enter your age ")
+
+// if ( age  < 18){
+//     console.log("You can not drive ")
+// }
+// else if (age > 18) {
+//     console.log("You can drive ")
+// }
+// else {
+//     console.log("ghlt input daal rhay ho , apni age daal bhai !!! ")
+// } // THis is a very simple program of (if else if )
