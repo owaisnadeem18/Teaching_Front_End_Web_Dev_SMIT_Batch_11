7fde7aa87cf5f5e5ef2970006661940af1615e8d
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2024-03-24 23:17:51 +0500

    Add files via upload

---
+++ Class 07 Functions Complete Explanation ( 22-03-2024 )/app.js
@@ -0,0 +1,95 @@
+Functions_Complete_Explanation_In_JavaScript;
+
+// // // // // Functions revision in javaScript
+
+// // // // // function definiton:
+
+// // // // // Function without passing any parameters
+
+// // // // function greet() {
+// // // //   alert("Welcome Dear User ");
+// // // // }
+
+// // // // // function calling
+// // // // greet();
+
+// // // // Function with passing any parameters or arguments
+
+// // // // jo function definition k andar may variables likh rha hota hn , wo meray pass dummy variables hotay hain , isko parameters kaha jaata hay
+
+// // // // function definiton
+
+// // // // parameters kehlata hay yahan ka variable
+// // // function sumKarwao(x, y) {
+// // //   add = x + y;
+// // //   return add;
+// // // }
+
+// // // // function calling
+
+// // // // is k andar variable key values , means k arguments ko pass karwaya jaye ga
+// // // // sumKarwao()
+
+// // // console.log(sumKarwao(5, 8));
+
+// // // defining the functon
+// // // //
+// // // function MultiplyKarwao(a, b) {
+// // //   multiply_Hua_Jawab = a * b;
+// // //   return multiply_Hua_Jawab;
+// // // }
+
+// // // // let's declare and initialize the variables
+
+// // // let pehla_variable = 4;
+// // // let dusra_variable = 6;
+
+// // // // calling the function
+
+// // // // ab yahan may arguments ko guzaar rha hn
+// // // console.log(MultiplyKarwao(pehla_variable, dusra_variable));
+
+// // function MultiplyKarwao(pehla_variable, dusra_variable) {
+// //   multiply_Hua_Jawab = pehla_variable * dusra_variable;
+// //   return multiply_Hua_Jawab;
+// // }
+
+// // // let's declare and initialize the variables
+
+// // let pehla_variable = 9;
+// // let dusra_variable = 3;
+
+// // // calling the function
+
+// // // ab yahan may arguments ko guzaar rha hn
+// // console.log(MultiplyKarwao(pehla_variable, dusra_variable));
+
+// // let a = 5;
+// // console.log(a)
+
+// // function sum(a,b) {
+// //   //   return num1 - num2;
+// //   let a = 5;
+// //   let b = 6;
+// //   console.log(a + b);
+// //   let add = a + b;
+// //   return a + b;
+// //   return a + b;
+// //   console.log(a + b);
+// // }
+
+// // let fun = sum(4, 5);
+// // alert(fun);
+// // console.log(fun);
+// // document.write(fun);
+
+// // id
+
+// function getHeading() {
+//   //   // innerText(just for heading, para, a , li , span)
+//   let heading = document.getElementById("heading").innerText;
+//   console.log(heading);
+//   //   let userName = document.getElementById("name").innerText;
+//   //   let userInput = document.getElementById("text").value;
+//   //   console.log(userInput);
+// }
